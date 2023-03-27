902b107cca49519810132005e6f473d06c8d6fa2
shadynoor <shadynoor9@gmail.com>
2023-03-27 12:26:25 +0200

    updated

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,11 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { DisplayedComponent } from './components/displayed/displayed.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 @NgModule({
-  declarations: [LoginComponent, DisplayedComponent, RegisterComponent],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [CommonModule, AuthRoutingModule, ReactiveFormsModule],
 })
 export class AuthModule {}
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home.component';
 
 @NgModule({
-  declarations: [NavbarComponent],
+  declarations: [NavbarComponent, HomeComponent],
   imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent],
+  exports: [NavbarComponent, HomeComponent],
 })
 export class LayoutModule {}
